b1cf08a104fe671181aea533ad0df39bf1c18cb1
sean-mbp <sean@sean-mbp>
2012-11-26 00:39:11 -0800

    Combine builds of all disks into this file.

--- naja/build/boot.bat
+++ naja/build/boot.bat
@@ -13,38 +13,76 @@ if not exist %BIN% mkdir %BIN%
 %ASM% NAJA\BOOT				NDOS.BOOT1.S
 %ASM% NAJA\BOOT				PR.INFO.S
 %ASM% NAJA\BOOT				SCREEN.III.S
-%ASM% NAJA\IO				NDOS.525.S
-%ASM% NAJA\IO				ASM.NCOMMONS.S
-%ASM% MOTHER\HALL			ASM.HALL.S
-%ASM% TRACC					ASM.CAMP.S
+%ASM% NAJA\IO				NDOS.525.S		-sym %BIN%\ndos.sym
+%ASM% NAJA\IO				ASM.NCOMMONS.S	-sym %BIN%\ncom.sym
+%ASM% MOTHER\HALL			ASM.HALL.S		-sym %BIN%\hall.sym
+%ASM% TRACC					ASM.CAMP.S		-sym %BIN%\camp.sym
 %ASM% NAJA\IO				ASM.CHAR.DATA.S
 %ASM% NAJA\BOOT				ASM.TITLE.S
-%ASM% MOTHER\GROUP.ASSEMBLY	ASM.GROUP.S
+%ASM% MOTHER\GROUP.ASSEMBLY	ASM.GROUP.S		-sym %BIN%\group.sym
 %ASM% MOTHER\ENROLLTEST		ASM.ENTEST.S
 %ASM% MOTHER\ENERGY			ASM.ENERGY.S
 %ASM% MOTHER\ARSENAL		ASM.ARSENAL.S
 %ASM% MOTHER\ROBOT.REPAIR	ASM.REPAIR.S
-%ASM% MOTHER\INFIRMARY		ASM.INFIRMARY.S
+%ASM% MOTHER\INFIRMARY		ASM.INFIRMARY.S	-sym %BIN%\infirmary.sym
 
-a2nib -create -volume 0 -disk %BIN%\boot.nib
-a2nib %OBJ%\NDOS.BOOT1		-t 00 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\PR.INFO			-t 00 -s 02 -disk %BIN%\boot.nib
-a2nib %OBJ%\SCREEN.III		-t 00 -s 03 -disk %BIN%\boot.nib
-a2nib %OBJ%\NDOS.525		-t 00 -s 04 -disk %BIN%\boot.nib
-a2nib %OBJ%\NCOMMONS		-t 00 -s 08 -disk %BIN%\boot.nib
-a2nib %OBJ%\CAMP.B000		-t 02 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\CAMP.E000		-t 03 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\CAMP.D000.M2	-t 05 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\CAMP.D000.M1	-t 06 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\TITLE.PAGE		-t 07 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\MTUNNELS		-t 0C -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\MCONTROL		-t 0E -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\MTESSINFO		-t 0F -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\CHAR.DATA		-t 10 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\NDOS.525W		-t 10 -s 0E -disk %BIN%\boot.nib
-a2nib %OBJ%\GROUP.ASSEMBLY	-t 11 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\ENROLL.TEST		-t 13 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\INFIRMARY		-t 17 -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\ENERGY.CENTER	-t 18 -s 08 -disk %BIN%\boot.nib
-a2nib %OBJ%\ARSENAL			-t 1B -s 00 -disk %BIN%\boot.nib
-a2nib %OBJ%\ROBOT.REPAIR	-t 1F -s 00 -disk %BIN%\boot.nib
+%ASM% FIGHT\LOADER			ASM.LOADER.S
+%ASM% FIGHT\COMMAND			ASM.COMMAND.S
+%ASM% FIGHT\NARRATOR		ASM.NARRATOR.S
+%ASM% FIGHT\AWARDER			ASM.AWARDER.S
+%ASM% FIGHT\DEAD			ASM.DEAD.S
+
+%ASM% ALIENS				ASM.DATA.S	-list ALIEN.DATA.LST
+%ASM% ALIENS				ASM.PICS.S	-list ALIEN.PICS.LST
+
+%ASM% TRACC					ASM.17.S
+%ASM% TRACC					ASM.15.S
+%ASM% TRACC					ASM.13.S
+%ASM% TRACC					ASM.11.S
+
+%ASM% TRACC					ASM.CAMP.S
+%ASM% TRACC					ASM.TUNNELS.S
+%ASM% TRACC\EDITOR			SELECT.OPTION.S
+%ASM% TRACC\EDITOR			EDITOR.S
+%ASM% SPECIALS\ELEVATOR		THE.ELEVATOR.S
+%ASM% SPECIALS\FINDMENT		FINDMENT.S
+
+set A2NIB=a2nib -disk %BIN%\boot.nib
+%A2NIB% -create -volume 0
+
+%A2NIB% %OBJ%\NDOS.BOOT1		-t 00 -s 00
+%A2NIB% %OBJ%\PR.INFO			-t 00 -s 02
+%A2NIB% %OBJ%\SCREEN.III		-t 00 -s 03
+%A2NIB% %OBJ%\NDOS.525			-t 00 -s 04
+%A2NIB% %OBJ%\NCOMMONS			-t 00 -s 08
+%A2NIB% %OBJ%\CAMP.B000			-t 02 -s 00
+%A2NIB% %OBJ%\CAMP.E000			-t 03 -s 00
+%A2NIB% %OBJ%\CAMP.D000.M2		-t 05 -s 00
+%A2NIB% %OBJ%\CAMP.D000.M1		-t 06 -s 00
+%A2NIB% %OBJ%\TITLE.PAGE		-t 07 -s 00
+%A2NIB% %OBJ%\MTUNNELS			-t 0C -s 00
+%A2NIB% %OBJ%\MCONTROL			-t 0E -s 00
+%A2NIB% %OBJ%\MTESSINFO			-t 0F -s 00
+%A2NIB% %OBJ%\CHAR.DATA			-t 10 -s 00
+%A2NIB% %OBJ%\NDOS.525W			-t 10 -s 0E
+%A2NIB% %OBJ%\GROUP.ASSEMBLY	-t 11 -s 00
+%A2NIB% %OBJ%\ENROLL.TEST		-t 13 -s 00
+%A2NIB% %OBJ%\INFIRMARY			-t 17 -s 00
+%A2NIB% %OBJ%\ENERGY.CENTER		-t 18 -s 08
+%A2NIB% %OBJ%\ARSENAL			-t 1B -s 00
+%A2NIB% %OBJ%\ROBOT.REPAIR		-t 1F -s 00
+
+set A2NIB=a2nib -disk %BIN%\tracc1.nib
+%A2NIB% -create -volume 2
+
+%A2NIB% %OBJ%\CONTROL17		-t 11 -s 00
+REM %A2NIB% %OBJ%\CONTROL15		-t 11 -s 00
+
+%A2NIB% %OBJ%\ELEVATOR		-t 14 -s 00
+%A2NIB% %OBJ%\FINDMENT		-t 15 -s 00
+%A2NIB% %OBJ%\CAMP.7800		-t 16 -s 00
+%A2NIB% %OBJ%\CAMP.B000		-t 18 -s 00
+%A2NIB% %OBJ%\TUNNELS		-t 19 -s 00
+%A2NIB% %OBJ%\ALIEN.EDITOR	-t 20 -s 08
+%A2NIB% %OBJ%\LOADER17		-t 22 -s 00
+%A2NIB% %OBJ%\SELECT.OPTION	-t 22 -s 0D
