4824d7eb9b55a1f73286ac39f30900d981614106
seanahan5000 <seanahan5000@gmail.com>
2018-10-27 11:21:20 -0700

    Add -c[ount] option for specifying expected number of sectors.

--- tools/a2nib/a2nib.cpp
+++ tools/a2nib/a2nib.cpp
@@ -3,15 +3,17 @@
 #include <stdio.h>
 #include "DiskImage.h"
 
-#define NEW_DOS		//***
+// always build new dos images now
+#define NEW_DOS
 
-int main(int argc,char* argv[])
+int main(int argc, char* argv[])
 {
 	const char* fileName = NULL;
 	const char* diskName = NULL;
 	INT32 volume = -1;
 	INT32 track = -1;
 	INT32 sector = -1;
+	INT32 count = 0;
 	bool create = false;
 	
 	argc -= 1;
@@ -24,7 +26,7 @@ int main(int argc,char* argv[])
 	{
 		char* str = *argv++;
 		
-		if (_stricmp(str,"-d") == 0 || _stricmp(str,"-disk") == 0)
+		if (_stricmp(str, "-disk") == 0)
 		{
 			if (argc-- == 0)
 			{
@@ -41,7 +43,7 @@ int main(int argc,char* argv[])
 			
 			diskName = str;
 		}
-		else if (_stricmp(str,"-v") == 0 || _stricmp(str,"-volume") == 0)
+		else if (_stricmp(str, "-volume") == 0)
 		{
 			if (argc-- == 0)
 			{
@@ -50,14 +52,14 @@ int main(int argc,char* argv[])
 			}
 			str = *argv++;
 			
-			sscanf(str,"%x",&volume);
+			sscanf(str, "%x", &volume);
 			if (volume < 0 || volume > 6)
 			{
 				printf("Invalid volume value\n");
 				return -1;
 			}
 		}
-		else if (_stricmp(str,"-t") == 0 || _stricmp(str,"-track") == 0)
+		else if (_stricmp(str, "-t") == 0 || _stricmp(str, "-track") == 0)
 		{
 			if (argc-- == 0)
 			{
@@ -66,14 +68,14 @@ int main(int argc,char* argv[])
 			}
 			str = *argv++;
 			
-			sscanf(str,"%x",&track);
+			sscanf(str, "%x", &track);
 			if (track < 0 || track >= 35)
 			{
 				printf("Invalid track value\n");
 				return -1;
 			}
 		}
-		else if (_stricmp(str,"-s") == 0 || _stricmp(str,"-sector") == 0)
+		else if (_stricmp(str, "-s") == 0 || _stricmp(str, "-sector") == 0)
 		{
 			if (argc-- == 0)
 			{
@@ -82,20 +84,36 @@ int main(int argc,char* argv[])
 			}
 			str = *argv++;
 			
-			sscanf(str,"%x",&sector);
+			sscanf(str, "%x", &sector);
 			if (sector < 0 || sector >= 16)
 			{
 				printf("Invalid sector value\n");
 				return -1;
 			}
 		}
-		else if (_stricmp(str,"-c") == 0 || _stricmp(str,"-create") == 0)
+		else if (_stricmp(str, "-c") == 0 || _stricmp(str, "-count") == 0)
+		{
+			if (argc-- == 0)
+			{
+				printf("Missing sector count value\n");
+				return -1;
+			}
+			str = *argv++;
+
+			sscanf(str, "%x", &count);
+			if (count < 0)
+			{
+				printf("Invalid sector count value\n");
+				return -1;
+			}
+		}
+		else if (_stricmp(str, "-create") == 0)
 		{
 			create = true;
 		}
 		else if (str[0] == '-')
 		{
-			printf("Unknown option \"%s\"\n\n",str);
+			printf("Unknown option \"%s\"\n\n", str);
 			goto usage;
 		}
 		else if (!fileName)
@@ -157,33 +175,40 @@ int main(int argc,char* argv[])
 	
 	if (!create)
 	{
-		file = fopen(fileName,"rb");
+		file = fopen(fileName, "rb");
 		if (!file)
 		{
-			printf("Unable to open \"%s\"\n",fileName);
+			printf("Unable to open \"%s\"\n", fileName);
 			return -1;
 		}
 		
-		fseek(file,0,SEEK_END);
+		fseek(file, 0, SEEK_END);
 		INT32 size = ftell(file);
-		fseek(file,0,SEEK_SET);
+		fseek(file, 0, SEEK_SET);
 		fileDataSize = (size + 255) & ~255;
 		fileData = (UINT8*)malloc(fileDataSize);
-		memset(fileData,0,fileDataSize);
-		if (fread(fileData,1,size,file) != size)
+		memset(fileData, 0, fileDataSize);
+		if (fread(fileData, 1, size, file) != size)
 		{
 			printf("Error reading source file\n");
 			goto error;
 		}
 		fclose(file);
+
+		if (count > 0 && fileDataSize > count * 256)
+		{
+			printf("\n### Source file too large for sector count (0x%x > 0x%x)\n\n",
+				fileDataSize, count * 256);
+			goto error;
+		}
 	}
-	
+
 #ifdef NEW_DOS
 	diskImage = new Naja2Image();
 #else
 	diskImage = new NajaImage();
 #endif
-	if (!diskImage->Open(diskName,create))
+	if (!diskImage->Open(diskName, create))
 	{
 		printf("Unable to open/create disk image file\n");
 		goto error;
@@ -191,7 +216,7 @@ int main(int argc,char* argv[])
 	
 	if (create)
 	{
-		printf("Initializing %s\n",diskName);
+		printf("Initializing %s\n", diskName);
 		diskImage->Initialize(volume);
 	}
 	else
@@ -200,8 +225,8 @@ int main(int argc,char* argv[])
 		vts.volume = volume;
 		vts.track = track;
 		vts.sector = sector;
-		printf("Writing %s\n",fileName);
-		if (!diskImage->Write(vts,fileData,fileDataSize))
+		printf("Writing %s\n", fileName);
+		if (!diskImage->Write(vts, fileData, fileDataSize))
 		{
 			printf("Write to disk image failed\n");
 			goto error;
@@ -224,7 +249,7 @@ error:
 	return -1;
 
 usage:
-	printf("a2nib -c[reate] -v[olume] <value> -d[isk] <disk-name>\n");
-	printf("a2nib <file-name> -t[rack] <hex> -s[ector] <hex> -d[isk] <disk-name>\n");
+	printf("a2nib -create -volume <value> -d[isk] <disk-name>\n");
+	printf("a2nib <file-name> -t[rack] <hex> -s[ector] <hex> [-c[ount] <hex>] -disk <disk-name>\n");
 	return -1;
 }
