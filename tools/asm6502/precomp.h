656de7965bea1839632027bfabfd91a8a19b45ac
Sean Callahan <sean@microsoft.com>
2019-06-03 15:29:56 -0700

    Stop including <windows.h>

--- tools/asm6502/precomp.h
+++ tools/asm6502/precomp.h
@@ -1,9 +1,17 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
-// TODO: get rid of windows dependencies
-#include <windows.h>
+#include <stdint.h>
 
+typedef int8_t		INT8;
+typedef uint8_t		UINT8;
+typedef int16_t		INT16;
+typedef uint16_t	UINT16;
+typedef int32_t		INT32;
+typedef uint32_t	UINT32;
+
+#include <stdlib.h>
+#include <stdarg.h> // for va_start
 #include <stdio.h>
 #include <string>
 #include <vector>
