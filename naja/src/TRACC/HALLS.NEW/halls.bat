69eddb2596620524c938158ee60703598d7391ad
Sean Callahan <seanahan5000@gmail.com>
2019-07-31 19:25:54 -0700

    Start of new experimental hall code.

---
+++ naja/src/TRACC/HALLS.NEW/halls.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set ROOT=%~dp0..\..\..
+set BUILD=%ROOT%\build
+set SRC=%ROOT%\src
+set OBJ=%ROOT%\obj
+set BIN=%ROOT%\bin
+set ASM=asm6502 -root %SRC% -objbase ..\obj -srcbase
+
+if not exist %OBJ% mkdir %OBJ%
+if not exist %BIN% mkdir %BIN%
+
+%ASM% TRACC\HALLS.NEW   ASM.HALLS.S    -list HALLS.LST -sym %BIN%\halls.sym
+
+dos33 -create -d %BIN%\HALLS.DSK
+dos33 -bsave %OBJ%\HALLS -d %BIN%\HALLS.DSK -a 6000
